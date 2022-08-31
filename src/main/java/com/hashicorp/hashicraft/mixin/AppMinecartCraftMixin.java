9971668e72a7ac78722d05705e19a8a7216ea84d
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2022-08-31 15:26:34 -0400

    Pass NBT tags to app minecart Not the greatest code. But hey, this is what happens when you modify non-extensible interfaces.

---
+++ src/main/java/com/hashicorp/hashicraft/mixin/AppMinecartCraftMixin.java
@@ -0,0 +1,36 @@
+package com.hashicorp.hashicraft.mixin;
+
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.recipe.ShapelessRecipe;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ShapelessRecipe.class)
+public abstract class AppMinecartCraftMixin {
+    @Shadow
+    @Final
+    private ItemStack output;
+
+    @Shadow
+    @Final
+    private Identifier id;
+
+    @Inject(method = "craft", at = @At("HEAD"))
+    public void interceptCraft(CraftingInventory craftingInventory, CallbackInfoReturnable<ItemStack> info) {
+        NbtCompound nbtList = new NbtCompound();
+        for (int i = 0; i < craftingInventory.size(); i++) {
+            ItemStack item = craftingInventory.getStack(i);
+            if (item.hasNbt()) {
+                nbtList.copyFrom(item.getNbt());
+            }
+        }
+        this.output.setNbt(nbtList);
+    }
+}
